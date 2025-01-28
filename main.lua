39578bc47c8336be625f48e68bfd53c65619e46f
Guilherme <guilhermevianinip@gmail.com>
2025-01-28 12:33:03 -0300

    feat: player 1 e 2 agora se movem

--- main.lua
+++ main.lua
@@ -10,12 +10,17 @@ VIRTUAL_HEIGHT = 243
 --[[tamanho da tela do jogo]]
 --[[a tela do jogo ainda será 1280/720 porém, para a estética de jogo antigo, é determinado um tamanho virtual que vai fazer o programa rodar em 432/243]]
 
+PADDLE_SPEED = 200
+--[[determina a velocidade do bastão baseando no tempo de atualização multiplicado pelo valor determinado]]
+
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
     
     -- texto retrô
     smallFont = love.graphics.newFont('font.ttf', 8)
     
+    scoreFont = love.graphics.newFont('font.ttf', 32)
+
     -- trocar fonte
     love.graphics.setFont(smallFont)
 
@@ -24,8 +29,32 @@ function love.load()
         resizable = false,
         vsync = true
     })
+    player1Score = 0
+    player2Score = 0
+    --iniciar pontuação zerada
+
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 50
 end
 --[[ os padroes de tamanho de tela não poderao ser alterados mas será sincronizado com o monitor]]
+
+function love.update(dt)
+    -- movimento do player 1
+    if love.keyboard.isDown('w') then
+        --valores positivos e negativos de movimento do eixo y
+        player1Y = player1Y + -PADDLE_SPEED * dt
+
+    elseif love.keyboard.isDown('s') then
+        player1Y = player1Y + PADDLE_SPEED * dt
+    end
+
+    --movimento do player 2
+    if love.keyboard.isDown('up') then
+        player2Y = player2Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('down') then
+        player2Y = player2Y + PADDLE_SPEED * dt
+    end
+end
 
 function love.keypressed(key)
     if key == 'escape'
@@ -39,13 +68,20 @@ function love.draw()
     -- limpa a tela com as cores rgb determinadas
     love.graphics.clear(0.15, 0.17, 0.20, 1)
 
-    love.graphics.printf('Hello Pong', 0, 20, VIRTUAL_WIDTH, 'center')-- troquei height por 20
+    love.graphics.setFont(smallFont)
+    love.graphics.printf('Hello Pong', 0, 20, VIRTUAL_WIDTH, 'center')
 
+    -- desenha pontuação dos dois no topo da tela
+    --precisa alterar a fonte para desenhar
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH/ 2 + 30, VIRTUAL_HEIGHT / 3)
+    
     --retangulo da esquerda
-    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
 
     --retangulo da direita
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
 
     --bola
     love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
