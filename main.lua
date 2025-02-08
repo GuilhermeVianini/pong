4801df906be6e70c98c50332d8e280995d173748
Guilherme <guilhermevianinip@gmail.com>
2025-02-08 15:47:09 -0300

    feat: bola move

--- main.lua
+++ main.lua
@@ -16,11 +16,10 @@ PADDLE_SPEED = 200
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
     
+    math.randomseed(os.time())
+
     -- texto retrô
     smallFont = love.graphics.newFont('font.ttf', 8)
-    
-    scoreFont = love.graphics.newFont('font.ttf', 32)
-
     -- trocar fonte
     love.graphics.setFont(smallFont)
 
@@ -29,12 +28,19 @@ function love.load()
         resizable = false,
         vsync = true
     })
-    player1Score = 0
-    player2Score = 0
-    --iniciar pontuação zerada
-
     player1Y = 30
     player2Y = VIRTUAL_HEIGHT - 50
+
+    --velocidade e posição da bola no começo
+    ballX = VIRTUAL_WIDTH /2 - 2
+    ballY = VIRTUAL_HEIGHT / 2 - 2
+
+    --altera a direção da bola
+    ballDX = math.random(2) == 1 and 100 or -100
+    ballDY = math.random(-50, 50)
+
+    -- variavel para identificar o estado do jogo "menu, jogo, lista de pontuação..."
+    gameState = 'start'
 end
 --[[ os padroes de tamanho de tela não poderao ser alterados mas será sincronizado com o monitor]]
 
@@ -42,23 +48,45 @@ function love.update(dt)
     -- movimento do player 1
     if love.keyboard.isDown('w') then
         --valores positivos e negativos de movimento do eixo y
-        player1Y = player1Y + -PADDLE_SPEED * dt
+        --agora as hastes não saem da tela
+        player1Y = math.max(0, player1Y + -PADDLE_SPEED *dt)
 
     elseif love.keyboard.isDown('s') then
-        player1Y = player1Y + PADDLE_SPEED * dt
+        player1Y = math.min(VIRTUAL_HEIGHT - 20, player1Y + PADDLE_SPEED * dt)
     end
 
     --movimento do player 2
     if love.keyboard.isDown('up') then
-        player2Y = player2Y + -PADDLE_SPEED * dt
+        player2Y = math.max(0, player2Y + -PADDLE_SPEED * dt)
     elseif love.keyboard.isDown('down') then
-        player2Y = player2Y + PADDLE_SPEED * dt
+        player2Y = math.min(VIRTUAL_HEIGHT - 20, player2Y + PADDLE_SPEED * dt)
+    end
+
+    --atualiza a bola bazeado no DX e DY se não estiver no começo
+    --escala a velocidade da bola por dt então depende de framerate
+    if gameState == 'play' then
+        ballX = ballX + ballDX * dt
+        ballY = ballY + ballDY * dt
     end
 end
 
 function love.keypressed(key)
     if key == 'escape'
         then love.event.quit()
+    elseif key == 'enter' or key == 'return' then
+        if gameState =='start' then
+            gameState = 'play'
+        else
+            gameState= 'start'
+            
+            --bola começa no meio
+            ballX = VIRTUAL_WIDTH / 2 - 2
+            ballY = VIRTUAL_HEIGHT / 2 - 2
+
+            --valor aleatorio de velocidade para a bola
+            ballDX = math.random(2) == 1 and 100 or -100
+            ballDY = math.random(-50, 50) * 1.5
+        end
     end
 end
 
@@ -69,13 +97,12 @@ function love.draw()
     love.graphics.clear(0.15, 0.17, 0.20, 1)
 
     love.graphics.setFont(smallFont)
-    love.graphics.printf('Hello Pong', 0, 20, VIRTUAL_WIDTH, 'center')
 
-    -- desenha pontuação dos dois no topo da tela
-    --precisa alterar a fonte para desenhar
-    love.graphics.setFont(scoreFont)
-    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
-    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH/ 2 + 30, VIRTUAL_HEIGHT / 3)
+    if gameState == 'start' then
+        love.graphics.printf('Hello Start State', 0, 20, VIRTUAL_WIDTH, 'center')
+    else
+        love.graphics.printf('Hello Play State', 0, 20, VIRTUAL_WIDTH, 'center')
+    end
     
     --retangulo da esquerda
     love.graphics.rectangle('fill', 10, player1Y, 5, 20)
@@ -84,7 +111,7 @@ function love.draw()
     love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
 
     --bola
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+    love.graphics.rectangle('fill', ballX, ballY, 4, 4)
 
     push:apply('end')
 end
